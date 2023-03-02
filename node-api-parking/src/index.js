24f8531957fc7c12d8c09d620c09361c42865bad
Angélica Shigematsu <78798754+angelica-shigematsu@users.noreply.github.com>
2023-03-02 19:34:56 -0300

    add connection of anywhere

--- node-api-parking/src/index.js
+++ node-api-parking/src/index.js
@@ -11,6 +11,18 @@ import { alterVehicle, createVehicle,
 
 const app = express();
 
+app.use((req, res, next) => {
+
+  res.header("Access-Control-Allow-Origin", "*");// dentro do '*' poderia ser qual site poderia fazer a requisiçao.
+
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+
+  res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+
+  next();
+
+})
+
 app.use(express.json());
 
 app.get('/api/vehicles', listVehicles);
