c33280e9bf0b0583bdff2b0f7641d9a9cbfa3e38
Angélica <angelica_shigematsu@outlook.com>
2023-04-01 22:31:47 -0300

    add exercicio5

---
+++ exercicio/exercicio5/calculoDaArea.js
@@ -0,0 +1,24 @@
+const pi = 3.14159;
+
+function calcularAreaCirculo() {
+  let raio = Number(document.getElementById('raio').value)
+
+  if(validarValor(raio)) {
+    let areaCirculo = calcularArea(raio);
+
+    document.getElementById("area").value =  areaCirculo.toFixed(3);
+  } else {
+    alert("Valores inválidos")
+  }
+}
+
+function calcularArea(raio) {
+  return pi * Math.pow(raio, 2);
+}
+
+function validarValor(valor) {
+  if (valor != null && !isNaN(valor)) 
+    return true
+
+  return false;
+}
