c0be6061bd2da6c4e58c4c38eb62fda60ce1f06c
Angélica <angelica_shigematsu@outlook.com>
2023-07-26 14:49:34 -0300

    feat: added to calculate total price

--- node-api-product/web/cart.js
+++ node-api-product/web/cart.js
@@ -3,15 +3,17 @@ import { products, order } from './mockProducts.js'
 
 window.addEventListener('load', () => {
   menuHorinzontal();
-  orderCart();
-})
+  orderCart(order);
+});
 
-const orderCart = () => {
+const orderCart = (cart) => {
+  let totalPriceByProduct = [];
   let product = []
-  order.forEach(productCart => {
+  cart.forEach(productCart => {
     product = products.filter(data => data.id == productCart.productId)
     document.getElementById('cart').innerHTML += 
     `<form action="" method="Post">
+      <input name="id" value=${product[0].id} hidden>
       <img src="${product[0].photo}" id="imgProduct"alt="">
       <div class="card-title">
         <h2>${product[0].name}</h2>
@@ -36,8 +38,27 @@ const orderCart = () => {
       </div>
       <img src="./assets/img/cart/delete.png" id="iconeDelete" alt="">
     </form>
-`;
-})
+    `;
+    totalPriceByProduct.push(product[0].price * productCart.quantity)
+  })
+  cardValueTotal(totalPrice(totalPriceByProduct));
 }
 
+const cardValueTotal = (total) => {
+  document.getElementById('card-price-total').innerHTML = 
+  `<div class="card-product-total">
+    <p class="price-total" id="total">R$${total}</p>
+  </div>
+  <div class="card-product-total">
+    <a href="#" class="link-calculate-frete">Calcular</a>
+  </div>
+  <div class="card-product-total">
+    <p class="price-total">R${total}</p>
+  </div>
+  `
+}
+
+const totalPrice = (sumByProduct) => {
+  return sumByProduct.reduce((price, amount) => price + amount).toFixed(2);
+}
 
