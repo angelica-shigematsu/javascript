8dfcb42d7bc604e43c94e7340dd959c1cfd62906
Angélica <angelica_shigematsu@outlook.com>
2023-07-26 14:52:39 -0300

    feat: redirect to menu

--- node-api-product/web/mockProducts.js
+++ node-api-product/web/mockProducts.js
@@ -1,18 +1,33 @@
 const products = [
   {
     id: 1,
-    name: "Pizza Marguerita",
-    description: "Muçarela, tomate, majericão , orégano",
-    price: 65.60,
-    photo: './assets/img/products/pizza.png'
+    name: "Torta de Leite Ninho",
+    description: "Massa de Baunilha; Creme de ninho Tradicional; Morango; Chantilly",
+    type: "torta",
+    price: 20.60,
+    photo: './assets/img/products/torta.png'
   },
   {
     id: 2,
-    name: "Torta de Leite Ninho",
-    description: "Massa de Baunilha; Creme de ninho Tradicional; Morango; Chantilly",
-    price: 65.60,
-    photo: './assets/img/products/torta.png'
+    name: "Pizza Marguerita",
+    description: "Muçarela, tomate, majericão , orégano",
+    type: "pizza",
+    price: 50.60,
+    photo: './assets/img/products/pizza.png'
+  },
+]
+
+const order = [
+  {
+    productId: 1,
+    quantity: 3
+  },
+  {
+    productId: 2,
+    quantity: 1
   }
 ]
 
-export default products
+export { 
+  products, 
+  order}
